5a606d1c74ca1ba1006ab6f0c1d556f575122edd
Ebla Ekerdiker <eblaekerdiker@gmail.com>
2023-05-07 21:56:32 +0300

    Completed Assignment I guess tests has a problem

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
-// Code your solutions in this file
+function writeCards(namesArray, eventName) {
+    let messageArray = []
+    for (let i = 0; i < namesArray.length; i++) {
+      messageArray.push(`Thank you, ${namesArray[i]}, for the wonderful ${eventName} gift!`);
+    }
+    return messageArray;
+  }
+  
+  console.log(writeCards(["Charlie", "Samip", "Ali"], "birthday"));
+
+  function countdown(number) {
+    let n = number;
+    while (n >= 0) {
+      console.log(n--);
+    }
+  }
